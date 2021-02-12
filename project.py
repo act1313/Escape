ef0fb6fd3d8700e21986266f2dc86a6aec7a1832
Aaron <aaronisawesome025@gmail.com>
2021-02-12 14:05:31 -0500

    fixed the clicking issue

--- project.py
+++ project.py
@@ -11,32 +11,46 @@ BLACK = (0, 0, 0)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Escape Room")
 BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'house.jpg')), (WIDTH, HEIGHT))
-RECYCLING_BIN = pygame.image.load(os.path.join('assets', 'bin.PNG'))
+RECYCLING_BIN = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'bin.PNG')), (40, 70))
+SINK = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'sink.png')), (50, 75))
 LIVES_FONT = pygame.font.SysFont("comicsans", 25)
 RIDDLE_FONT = pygame.font.SysFont("comicsans", 27)
 
 
-def draw_window(items):
-    WIN.blit(BACKGROUND, (0, 0))
-    # Rendering the font into displayable text
-    riddle_text = RIDDLE_FONT.render("Click the things that are wrong and that don't help global warming.", 1, BLACK)
-    lives_text = LIVES_FONT.render(f"Items left: {items}", 1, BLACK)
-
-    # Making the text show on the screen
-    WIN.blit(riddle_text, (WIDTH // 2 - riddle_text.get_width() // 2, 10))
-    WIN.blit(lives_text, (WIDTH - 125, 10))
-
-    pygame.display.update()
-
-
 # the main game loop for all of the events
 def main():
-    area = pygame.Rect(100, 150, 200, 124)
+
+    binArea = pygame.Rect(WIDTH - RECYCLING_BIN.get_width(), HEIGHT - RECYCLING_BIN.get_height(), 40, 70)
+    sinkArea = pygame.Rect(640, 220, 50, 75)
+
+    def draw_items():
+        # render the images to display
+        WIN.blit(RECYCLING_BIN, (WIDTH - RECYCLING_BIN.get_width(), HEIGHT - RECYCLING_BIN.get_height()))
+        WIN.blit(SINK, (640, 220))
+
+
+    def window():
+        WIN.blit(BACKGROUND, (0, 0))
+        # Rendering the font into displayable text
+        riddle_text = RIDDLE_FONT.render("Click the things that are wrong and that don't help global warming.", 1, BLACK)
+        lives_text = LIVES_FONT.render(f"Items left: {items}", 1, BLACK)
+
+        # Making the text show on the screen
+        WIN.blit(riddle_text, (WIDTH // 2 - riddle_text.get_width() // 2, 10))
+        WIN.blit(lives_text, (WIDTH - 125, 10))
+
+        draw_items()
+
+        # showing the images on the screen
+        pygame.display.update()
+
+
     run = True
     items = 3
     clock = pygame.time.Clock()
     while run:
         clock.tick(FPS)
+        window()
         # looping through a list of the different events
         for event in pygame.event.get():
             # the game is able to close when we hit the close button
@@ -44,10 +58,11 @@ def main():
                 run = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
-                    if area.collidepoint(event.pos):
-                        print("clicked")
-  
-        draw_window(items)
+                    if binArea.collidepoint(event.pos):
+                        print("bin")
+                        
+                    if sinkArea.collidepoint(event.pos):
+                        print("sink")
 
     pygame.quit()
 
