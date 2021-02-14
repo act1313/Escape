2c3d1d55e53bb03d272cc47630050344117a9454
Aaron <aaronisawesome025@gmail.com>
2021-02-13 19:56:24 -0500

    starting to refactor for second level

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'ho
 RECYCLING_BIN = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'bin.PNG')), (40, 70))
 SINK = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'sink.png')), (50, 75))
 LIVES_FONT = pygame.font.SysFont("comicsans", 25)
-RIDDLE_FONT = pygame.font.SysFont("comicsans", 27)
+RIDDLE_FONT = pygame.font.SysFont("comicsans", 28)
 
 
 # the main game loop for all of the events
 def main():
-
+    # the hitboxes for the areas
     binArea = pygame.Rect(WIDTH - RECYCLING_BIN.get_width(), HEIGHT - RECYCLING_BIN.get_height(), 40, 70)
     sinkArea = pygame.Rect(640, 220, 50, 75)
     lightoneArea = pygame.Rect(375, 100, 190, 35)
@@ -31,22 +31,22 @@ def main():
         WIN.blit(RECYCLING_BIN, (WIDTH - RECYCLING_BIN.get_width(), HEIGHT - RECYCLING_BIN.get_height()))
         WIN.blit(SINK, (640, 220))
 
-
     def window():
         WIN.blit(BACKGROUND, (0, 0))
         # Rendering the font into displayable text
-        riddle_text = RIDDLE_FONT.render("Click the things that are wrong and that don't help global warming.", 1, BLACK)
+        riddle_text = RIDDLE_FONT.render("This family left for the day. Click the things that are", 1,BLACK)
+        riddle_second_half_text = RIDDLE_FONT.render(" incorrect and don't help climate change.", 1, BLACK)
         lives_text = LIVES_FONT.render(f"Items left: {items}", 1, BLACK)
 
         # Making the text show on the screen
         WIN.blit(riddle_text, (WIDTH // 2 - riddle_text.get_width() // 2, 10))
+        WIN.blit(riddle_second_half_text, (WIDTH // 2 - riddle_second_half_text.get_width() // 2, 50))
         WIN.blit(lives_text, (WIDTH - 125, 10))
 
         draw_items()
 
         # showing the images on the screen
         pygame.display.update()
-
 
     run = True
     items = 5
